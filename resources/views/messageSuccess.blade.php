cdf927fde8773eb11b1b3e68f2eee25bd011fb7e
Steph118 <stephaneatsu18@gmail.com>
2022-07-25 15:34:51 +0000

    modification du contenue de suuccess_blade

--- resources/views/messageSuccess.blade.php
+++ resources/views/messageSuccess.blade.php
@@ -3,7 +3,7 @@
 
 <div class=" container php-email-form">
     <div class="py-5 text-center ">
-    <h4 class=" text-success ">votre message a bien été envoyé</h4>
+    <h4 class=" text-success ">Votre message a bien été envoyé !</h4>
         <a href="{{route('welcome')}}" class="text-black" style="text-decoration: underline"><i>retour à l'accueil</i></a>
     </div>
 </div>
